44321050f1f717eeddff2e412350a0cac9b17c25
Eugen Guriev <gurievcreative@gmail.com>
2020-05-05 19:38:23 +0300

    travers, toArray, sizeOfTree

--- binaryTree.js
+++ binaryTree.js
@@ -73,11 +73,57 @@ function distanceBetween (root, from, to) {
   }
 }
 
+/**
+ * Travers.
+ *
+ * @param {node} tree Whole tree.
+ * @param {Function} fn Callback.
+ */
+function traverse (tree, fn) {
+  if (tree !== null) {
+    tree.left = traverse(tree.left, fn)
+    tree.right = traverse(tree.right, fn)
+    tree.key = fn(tree.key, tree)
+  }
+  return tree
+}
+
+/**
+ * Convert binary tree to array.
+ *
+ * @param {node} tree Whole tree.
+ */
+function toArray (tree) {
+  let result = []
+  if (tree !== null) {
+    result = result.concat(tree.key)
+    result = result.concat(toArray(tree.left))
+    result = result.concat(toArray(tree.right))
+  }
+  return result
+}
+
+/**
+ * Function to return the size of the tree.
+ *
+ * @param {node} tree root node.
+ */
+function sizeOfTree (tree) {
+  if (tree === null) {
+    return 0
+  }
+
+  return sizeOfTree(tree.left) + sizeOfTree(tree.right) + 1
+} 
+
 const exmpl = [4, 7, 3, 1, 8, 2, 6, 5].reduce(
   (root, key) => insert(root, key),
   null
 )
 
 console.log('Binary Tree', JSON.stringify(exmpl))
+console.log('Size of tree', sizeOfTree(exmpl))
+console.log('Traverse',  JSON.stringify(traverse(exmpl, key => key * 2)))
+console.log('To array',  toArray(exmpl))
 console.log('Distance from root to 2', distanceFromRoot(exmpl, 2))
 console.log('Distance between 7 and 5', distanceBetween(exmpl, 7, 5))
